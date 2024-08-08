c4e00e3093b373a7b77a908d13e9a4e7666fd08d
AshleyEvonne <ashleyevonne345@gmail.com>
2024-08-08 09:21:43 -0400

    Generics Methods

---
+++ src/DemoClass.java
@@ -0,0 +1,8 @@
+public class DemoClass {
+    //Create Generics Method
+
+    public <T> void genericsMethod(T data){
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+}
